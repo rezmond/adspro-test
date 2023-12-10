606f871ad4fe3c745e8516863ff6b8646f123dfe
rezmond <test@gmail.com>
2023-12-10 14:25:32 +0400

    Introduce the "MaterialUI"

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,11 @@
 import { type FC } from 'react';
+import { Stack } from '@mui/material';
 
 import styles from './App.module.css';
 
-export const App: FC = () => <div className={styles.root}>Test</div>;
+export const App: FC = () => (
+  <Stack direction="row" gap={1} className={styles.root}>
+    <span>Test</span>
+    <span>Page</span>
+  </Stack>
+);
