7774819b077a92712026b35c8c6ea3ec4265293f
rezmond <test@gmail.com>
2023-12-11 20:41:20 +0400

    Add aside part of page layout

--- src/components/PageLayout/PageLayout.tsx
+++ src/components/PageLayout/PageLayout.tsx
@@ -1,12 +1,19 @@
-import { AppBar, Toolbar } from '@mui/material';
+import { AppBar, Grid, Stack, Toolbar } from '@mui/material';
 import { ReactNode, type FC } from 'react';
+
+import { useUpSm } from '@/shared/hooks';
 
 import { SearchInput } from '../SearchInput';
 import { MainMenu } from '../MainMenu';
 
 import styles from './PageLayout.module.css';
 
-type PageLayoutProps = { className?: string; bottomToolbar?: ReactNode };
+type PageLayoutProps = {
+  className?: string;
+  bottomToolbar?: ReactNode;
+  children: ReactNode;
+  asideContent?: ReactNode;
+};
 
 const menu = [
   { url: '/profile', label: 'Профиль' },
@@ -20,12 +27,33 @@ const mainMenuLabel = 'Main navigation';
 export const PageLayout: FC<PageLayoutProps> = ({
   className,
   bottomToolbar,
-}) => (
-  <AppBar className={className}>
-    <Toolbar className={styles.toolbar}>
-      <SearchInput className={styles.searchInput} />
-      <MainMenu className={styles.mainMenu} label={mainMenuLabel} menu={menu} />
-    </Toolbar>
-    {bottomToolbar}
-  </AppBar>
-);
+  children,
+  asideContent,
+}) => {
+  const isUpSm = useUpSm();
+  return (
+    <Stack direction="column" gap={2}>
+      <AppBar position="sticky" className={className}>
+        <Toolbar className={styles.toolbar}>
+          <MainMenu
+            className={styles.mainMenu}
+            label={mainMenuLabel}
+            menu={menu}
+          />
+          <SearchInput className={styles.searchInput} />
+        </Toolbar>
+        {bottomToolbar}
+      </AppBar>
+      <Grid container direction="row" gap={2}>
+        <Grid item flexGrow={1}>
+          {children}
+        </Grid>
+        {isUpSm && (
+          <Grid item className={styles.asideContent}>
+            {asideContent}
+          </Grid>
+        )}
+      </Grid>
+    </Stack>
+  );
+};
--- src/pages/ProductList/ProductList.tsx
+++ src/pages/ProductList/ProductList.tsx
@@ -6,5 +6,11 @@ import { BottomToolbar } from '@/components/BottomToolbar';
 type ProductListProps = { className?: string };
 
 export const ProductList: FC<ProductListProps> = ({ className }) => (
-  <PageLayout className={className} bottomToolbar={<BottomToolbar />} />
+  <PageLayout
+    className={className}
+    bottomToolbar={<BottomToolbar />}
+    asideContent="test"
+  >
+    {null}
+  </PageLayout>
 );
